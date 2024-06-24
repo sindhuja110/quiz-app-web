0129f5af194c58a2cb596a82a6d7c3e049f1f7a9
sindhuja110 <sindhuja1106@gmail.com>
2024-06-24 16:39:36 +0530

    added footer page

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,24 +1,24 @@
 // src/Footer.js
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
+import logoimage from '../Assets/companylogo.png';
 
 const Footer = () => {
     return (
         <footer className="footer">
             <Container>
                 <Row>
-                    <Col md={3} className="footer-section">
-                        <img src="socrative-logo.png" alt="Socrative Logo" className="logo" />
-                        <p>© Copyright 2024 Showbie Inc. All rights reserved.</p>
-                        <p>Socrative is a product of <strong>Showbie Inc.</strong></p>
+                    <Col md={3} className="footer-section border-right text-center">
+                        <img src={logoimage} alt="Driftmark Logo" className="logo" />
+                        <p><strong>Driftmark Technology</strong></p>
+                        <p>© Copyright 2024 Driftmark Technology. All rights reserved.</p>
                     </Col>
-                    <Col md={2} className="footer-section border-right">
+                    <Col md={2} className="footer-section border-right ">
                         <h4>Company</h4>
                         <ul>
                             <li><a href="#about">about us</a></li>
                             <li><a href="#privacy">privacy policy</a></li>
                             <li><a href="#terms">terms & conditions of service</a></li>
-                            <li><a href="#accessibility">accessibility declaration</a></li>
                         </ul>
                     </Col>
                     <Col md={2} className="footer-section border-right">
@@ -36,13 +36,13 @@ const Footer = () => {
                             <li><a href="#feature">request a feature</a></li>
                         </ul>
                         <div className="social-icons">
-                            <a href="https://twitter.com"><i className="fab fa-twitter"></i></a>
-                            <a href="https://facebook.com"><i className="fab fa-facebook"></i></a>
-                            <a href="https://youtube.com"><i className="fab fa-youtube"></i></a>
+                            <a href="https://twitter.com"><i className="fab fa-whatsapp outline-light"></i></a>
+                            <a href="https://facebook.com"><i className="fas fa-phone-alt outline-light"></i></a>
+                            <a href="https://youtube.com"><i className="fas fa-envelope outline-light"></i></a>
                         </div>
                     </Col>
-                    <Col md={3} className="footer-section last-section">
-                        <Button variant="outline-light" className="app-button">Get the app</Button>
+                    <Col md={2} className="footer-section last-section">
+                        <Button variant="outline-light" className="app-button">Get the app</Button><br/>
                         <Button variant="outline-light" className="pro-button">Upgrade to PRO</Button>
                     </Col>
                 </Row>
